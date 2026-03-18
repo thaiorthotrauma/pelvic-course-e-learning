781200b833c1f1a98e352b55c8f80e2a3004ca50
pongpoti <pong.poti@gmail.com>
2026-03-18 16:16:57 +0000

    c

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = process.env.PORT || 8080
 app.use("/", express.static("public"))
 app.use("/privacy", express.static("privacy"))
-app.use("/tos", express.static("tos"))
+app.use("/terms", express.static("terms"))
 app.listen(port, () => { console.log("tots-pelvic-course-e-learning is live") })
